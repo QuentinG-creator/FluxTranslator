8e55ec5a631ecea6d06e59f642f4ac3e873a0293
QuentinG-creator <73114129+QuentinG-creator@users.noreply.github.com>
2024-05-14 16:50:31 +0200

    Updates

--- precache-manifest.09f73583bf610447739b3357d04b12b7.js
+++ precache-manifest.09f73583bf610447739b3357d04b12b7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21694dc15de2b0927ce42c2be8e68480",
+    "revision": "127e95d893ab4b276336c2b17ad5fa6d",
     "url": "/FluxTranslator/index.html"
   },
   {
-    "revision": "9df8738effcd56369296",
+    "revision": "c6242930cc343797cd80",
     "url": "/FluxTranslator/static/css/main.67bf931e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FluxTranslator/static/js/2.a2b181a3.chunk.js"
   },
   {
-    "revision": "9df8738effcd56369296",
-    "url": "/FluxTranslator/static/js/main.469674e1.chunk.js"
+    "revision": "c6242930cc343797cd80",
+    "url": "/FluxTranslator/static/js/main.d172be4f.chunk.js"
   },
   {
     "revision": "be11c8d34c2d5ab0f177",
